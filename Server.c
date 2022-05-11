e1b2b690f27560e18cc6e8ff698236927ef01f22
Sitanshu <sitanshudas2001@gmail.com>
2022-05-11 21:26:03 +0530

    Update Server.c

--- Server.c
+++ Server.c
@@ -7,34 +7,37 @@
 #include <string.h>
 
 int main (int argc, char *argv[]){
-    int n;
-    char data[201];
-    //create a socket
-    int net_socket;
-    net_socket = socket(AF_INET, SOCK_STREAM, 0);
+    
+    // Create a socket..
+    int request_socket, response_socket;
+    request_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    //connect to a server
-    //where we want to connect to
+    // Connect to a server where we want to get connected..
     struct sockaddr_in server_address, client_address;
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(5600);
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
 
-    bind(net_socket, (struct sockaddr*)&server_address,sizeof(server_address));
-
-    listen(net_socket,10);
-    time_t tick;
+	// Binding the socket and the port..
+    bind(request_socket, (struct sockaddr *) &server_address, sizeof(server_address));
+ 
+	// Listen: create queue to store requests..
+    listen(request_socket, 10);
+	
+	// Read, write operations..
+	char data[201];
+    int client_size, n;
     char str[100];
-    int fd;
-    int c;
+    time_t tick;
+	
     while (1){
-	    fd=accept(net_socket,(struct sockaddr*)&client_address,&c);
+	    response_socket = accept(request_socket, (struct sockaddr *) &client_address, &client_size);
 	    printf("Accepted");
-	    tick=time(NULL);
-	    snprintf(str,sizeof(str),"%s",ctime(&tick));
-	    printf("%s",str);
-	    write(fd,str,strlen(str));
+	    tick = time(NULL);
+	    snprintf(str, sizeof(str), "%s", ctime(&tick));
+	    printf("%s", str);
+	    write(response_socket, str, strlen(str));
     }
 
-    return(0);
+    return 0;
 }
