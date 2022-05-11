de4b9613e484dbd88a72fc33873127922253074d
Sitanshu <sitanshudas2001@gmail.com>
2022-05-11 21:15:41 +0530

    Update Client.c

--- Client.c
+++ Client.c
@@ -6,30 +6,26 @@
 int main (int argc, char *argv[]){
 
 	// Create a socket..
-	
 	int net_socket;
-	net_socket = socket (AF_INET, SOCK_STREAM, 0);      // IPV4, CONNECTION, PROTOCOL; RETURN TYPE: INT
+	net_socket = socket (AF_INET, SOCK_STREAM, 0);
 	
 	// Store the server details..
-	
 	struct sockaddr_in server;
-	server.sin_family = AF_INET;       // Family of the server.
-	server.sin_port = htons(5600);       // Port number of the server.
-	server.sin_addr.s_addr = inet_addr(argv[1]);   // ip address of the server.
+	server.sin_family = AF_INET;
+	server.sin_port = htons(5600);
+	server.sin_addr.s_addr = inet_addr(argv[1]);
 
 	// Connect to the server..
-	
-	connect(net_socket, (struct sockaddr *) &server, sizeof(server));  // Socket(file name), , size.
+	connect(net_socket, (struct sockaddr *) &server, sizeof(server));
 
-	char data[201];
+	// Read, write operations..
+	char data[200];
 	int n;
 	n = recv(net_socket, data, 200, 0);
-
 	printf("Reply from the Server: %s\n", data);
 	
 	// Closing the file descriptor..
-	
 	close(net_socket);
+	
 	return 0;
-
 }
